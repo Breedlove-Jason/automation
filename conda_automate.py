77ca19250b93afa9915e7485322235e179b4c53a
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-13 15:18:43 -0700

    add combo box so user can choose their shell for conda to initialize and activate properly

--- conda_automate.py
+++ conda_automate.py
@@ -1,6 +1,7 @@
+import os
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
-    QFileDialog, QTextEdit
+    QFileDialog, QTextEdit, QErrorMessage, QComboBox
 from PyQt5.QtCore import pyqtSignal, Qt
 from colorama import init
 
@@ -15,24 +16,40 @@ class JupyterManager(QWidget):
         self.env_input = QLineEdit()
         self.env_input.setStyleSheet(
             "background-color: #9D73FA; font-weight: bold;")
+        self.env_input.setPlaceholderText("Enter environment name")
 
         self.pkg_label = QLabel('Additional Packages')
         self.pkg_label.setStyleSheet("font-weight: bold")
         self.pkg_input = QLineEdit()
         self.pkg_input.setStyleSheet(
             "background-color: #9D73FA; font-weight: bold;")
+        self.pkg_input.setPlaceholderText(
+            "Enter additional packages (comma-separated)")
 
         self.port_label = QLabel('Port Number')
         self.port_label.setStyleSheet("font-weight: bold")
         self.port_input = QLineEdit()
         self.port_input.setStyleSheet(
             "background-color: #9D73FA; font-weight: bold;")
+        self.port_input.setPlaceholderText("Enter port number")
+
+        self.shell_label = QLabel('Shell')
+        self.shell_label.setStyleSheet("font-weight: bold")
+        self.shell_dropdown = QComboBox()
+        self.shell_dropdown.setStyleSheet(
+            "background-color: #9D73FA; font-weight: bold;")
+        self.shell_dropdown.addItem("bash")
+        self.shell_dropdown.addItem("cmd.exe")
+        self.shell_dropdown.addItem("powershell")
+        self.shell_dropdown.addItem("zsh")
+        self.shell_dropdown.addItem("fish")
 
         self.conda_label = QLabel('Conda Path')
         self.conda_label.setStyleSheet("font-weight: bold")
         self.conda_input = QLineEdit()
         self.conda_input.setStyleSheet(
             "background-color: #9D73FA; font-weight: bold;")
+        self.conda_input.setPlaceholderText("Enter path to conda executable")
 
         self.init_ui()
 
@@ -70,6 +87,9 @@ class JupyterManager(QWidget):
         form_layout.addWidget(self.port_label)
         form_layout.addWidget(self.port_input)
         form_layout.addWidget(self.conda_label)
+        # Add shell label and dropdown box to layout
+        form_layout.addWidget(self.shell_label)
+        form_layout.addWidget(self.shell_dropdown)
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.conda_input)
@@ -100,13 +120,35 @@ class JupyterManager(QWidget):
         self.show()
 
     def create_env_and_launch_jupyter(self):
-        pass
+        env_name = self.env_input.text()
+        additional_packages = self.pkg_input.text()
+        port_number = self.port_input.text()
+        conda_path = self.conda_input.text()
+        # Retrieve the selected shell from the dropdown box
+        selected_shell = self.shell_dropdown.currentText()
+
+        # Initialize Conda
+        init_conda_cmd = f'source {conda_path} && conda init {selected_shell} && conda activate {env_name}'
 
     def kill_jupyter_server(self):
         pass
 
     def browse_for_conda(self):
-        pass
+        # Open file dialog to select conda executable
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file, _ = QFileDialog.getOpenFileName(
+            self, "Select Conda Executable", "", "Executable Files (*.exe *.sh *.bat)", options=options)
+
+        if file == "":
+            return
+        if not os.path.isfile(file):
+            error_dialog = QErrorMessage(self)
+            error_dialog.showMessage('Invalid file selected')
+            return
+
+        # Set the selected file path in the conda_input QLineEdit
+        self.conda_input.setText(file)
 
 
 if __name__ == '__main__':
